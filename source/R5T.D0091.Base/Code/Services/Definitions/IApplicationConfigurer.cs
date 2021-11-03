e47de13c2b364639d3fece61eebfd068a50e98e8
David Coats <david@getsafetycone.com>
2021-11-03 16:05:45 -0700

    Add IApplicationConfigurer service definition.

---
+++ source/R5T.D0091.Base/Code/Services/Definitions/IApplicationConfigurer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+
+using R5T.T0064;
+
+
+namespace R5T.D0091
+{
+    [ServiceDefinitionMarker]
+    public interface IApplicationConfigurer : IServiceDefinition
+    {
+        Task ConfigureApplication(IApplicationBuilder applicationBuilder);
+    }
+}
